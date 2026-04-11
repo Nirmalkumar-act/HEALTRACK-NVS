a198a43edbaaee588db83f9315276bc4a222bad9
Nirmalkumar R <nirmalkumar76950@gmail.com>
2026-04-11 18:06:07 +0530

    fix: exclude DataSourceAutoConfiguration, remove conflicting spring.datasource.url, clean pom.xml

--- backend/src/main/java/com/healtrack/hmsbackend/HmsBackendApplication.java
+++ backend/src/main/java/com/healtrack/hmsbackend/HmsBackendApplication.java
@@ -2,8 +2,13 @@ package com.healtrack.hmsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,       // We provide our own DataSource in DataSourceConfig.java
+    SqlInitializationAutoConfiguration.class  // Prevents SQL init errors at startup
+})
 public class HmsBackendApplication {
 
     public static void main(String[] args) {
